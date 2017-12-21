054ae312a068bad1777c9849f3fb87ffd7b8d3a8
Brooks Patola <abpatola@myseneca.ca>
2017-12-20 19:44:19 -0500

    Update http.ts

--- src/providers/http/http.ts
+++ src/providers/http/http.ts
@@ -1,37 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-
-/*
-  Generated class for the HttpProvider provider.
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
-export class HttpProvider {
 
+export class HttpProvider {
+  eventData: any;
   api: string = 'http://app.toronto.ca/cc_sr_v1_app/data/edc_eventcal_APR';
 
   constructor(public http: HttpClient) {
     console.log('Hello HttpProvider Provider');
+    this.http.get(this.api)
+    .subscribe(data => {
+        this.eventData = data;
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  getJsonData(){
+    return this.eventData;
   }
   
-/*
-  http.get is used to fetch data from web service and map is used to manipulate data. 
-  Json() is called on the response to return data.
-*/
-
-getJsonData(){
-  return new Promise(resolve => {
-    this.http.get(this.api).subscribe(data => {
-      resolve(data);
-      console.log(data);
-    }, err => {
-      console.log(err);
-     
-    });
-  });
-
-}
 
 }
